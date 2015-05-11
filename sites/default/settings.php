5ae67edda8c565abe5d26572b1594fe58ce97fde
John Franti & Geoff Winner <student@epicodus.com>
2015-05-11 15:49:11 -0700

    add initial site data

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'db1',
+      'username' => 'john',
+      'password' => 'drupal',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'qMErBaxSFaIjA21k61qiG1SVWZ1OB3nAp3fGzlVsej8';
 
 /**
  * Base URL (optional).
